6f1d4df7310f6c0c57d23b5f5d2f49658e04fb01
RodriDesmond <c.rodri92@gmail.com>
2021-08-31 18:38:06 -0300

    Update production.py

--- settings/production.py
+++ settings/production.py
@@ -2,18 +2,19 @@ from .base import *
 import django_heroku
 import dj_database_url
 
-INSTALLED_APPS = [
-    'django_heroku',
-]
-
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: update this when you have the production host
-ALLOWED_HOSTS = ['0.0.0.0', 'localhost']
+ALLOWED_HOSTS = ['pregunchaco.herokuapp.com']
 
-
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': os.path.join(BASE_DIR,'db.sqlite3'),
+    }
+}
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATICFILES_DIRS = ( os.path.join(BASE_DIR,'static'),)
@@ -21,6 +22,4 @@ STATICFILES_DIRS = ( os.path.join(BASE_DIR,'static'),)
 MEDIA_URL = '/media/'
 MEDIA_ROOT = os.path.join(BASE_DIR,'media')
 LOGIN_REDIRECT_URL = '/'
-
-django_heorku.setting(locals())
 
