1fd359970d69d5b1240b9c4d264528acbb0b2839
RodriDesmond <c.rodri92@gmail.com>
2021-08-31 19:53:29 -0300

    actualizadas urls

--- apps/resultado/urls.py
+++ apps/resultado/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import *
 
 app_name = 'apps.resultado'
@@ -10,4 +8,4 @@ urlpatterns = [
     path('api/pregunchaco/puntos',validar_puntos,name='Puntaje conseguido'),
     path('api/pregunchaco/scoreboard',scoreboard,name="scoreboard"),
     path('ranking',view_ranking, name="ranking" ),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
--- apps/trivia/urls.py
+++ apps/trivia/urls.py
@@ -1,6 +1,4 @@
 from django.urls import path
-from django.conf import settings
-from django.conf.urls.static import static
 from .views import *
 
 app_name = 'apps.trivia'
@@ -10,4 +8,4 @@ urlpatterns = [
     path('categorias', elegir_categorias, name='categorias'),
     path('api/pregunchaco/',empezar_pregunchaco,name="api_preguntas"),
     path('pregunchaco/',pregunchaco, name="pregunchaco"),
-]+ static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
+]
