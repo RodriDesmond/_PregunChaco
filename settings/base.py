75e099fd832ab2fde028ca3a4f09a1155c01c517
RodriDesmond <c.rodri92@gmail.com>
2021-08-31 18:56:31 -0300

    Update base.py

--- settings/base.py
+++ settings/base.py
@@ -29,7 +29,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-+g#!ki4#anzhib_$-d)-uzlk4yb%74z+s1dd3i^g3fsd#jg8$c'
 
 
 # Application definition
