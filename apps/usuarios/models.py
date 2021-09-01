623a4431258f2106ecff952d7a5ae517ffa8a7eb
RodriDesmond <c.rodri92@gmail.com>
2021-09-01 10:50:45 -0300

    Agregado default_profile_pic

--- apps/usuarios/models.py
+++ apps/usuarios/models.py
@@ -7,7 +7,7 @@ class Perfil(models.Model):
     user = models.OneToOneField(User, null=True, on_delete=models.CASCADE)
     creado = models.DateField(auto_now_add=True)
     updated = models.DateField(auto_now_add=True)
-    imagen_perfil = models.ImageField(null=True, blank=True, upload_to="perfil/")
+    imagen_perfil = models.ImageField(null=True, blank=True, upload_to="perfil/", default='default_profile_pic.png')
     facebook_url = models.TextField(max_length=250, null=True, blank=True)
     twitter_url = models.TextField(max_length=250, null=True, blank=True)
     instagram_url = models.TextField(max_length=250, null=True, blank=True)
